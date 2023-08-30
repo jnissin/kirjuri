2fb8263f7d0eb05c800b73f80ae83e14388ebcf7
Joonas Nissinen <joonas.nissinen@futurice.com>
2023-08-30 17:05:06 +0300

    Implemented first working version of GPU based workflow and transcription test script

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,39 @@
-FROM python:3.9-slim
+# Use an official CUDA runtime as a parent image
+FROM nvidia/cuda:11.7.1-base-ubuntu20.04
 
 ENV PYTHONUNBUFFERED=1
+ENV DEBIAN_FRONTEND=non-interactive
 
-RUN apt-get update && apt-get install -y tini git
+# Install system packages and Python 3.10, set python3.10 and pip3.10 as defaults
+RUN apt-get update -y && apt-get install -y \
+    software-properties-common \
+    && add-apt-repository ppa:deadsnakes/ppa \
+    && apt-get update -y \
+    && apt-get install -y \
+        python3.10 \
+        python3.10-distutils \
+        ffmpeg \
+        git \
+        wget \
+        tini \
+    && wget https://bootstrap.pypa.io/get-pip.py \
+    && python3.10 get-pip.py \
+    && rm get-pip.py \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* \
+    && ln -sf /usr/bin/python3.10 /usr/bin/python3 \
+    && ln -sf $(which pip) /usr/local/bin/pip3 \
+    && ln -sf $(which pip) /usr/local/bin/pip3.10
+
+# Set the working directory
 WORKDIR /app
 
-# Install dependencies
+# Install Python dependencies
 COPY requirements.txt /app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install --upgrade --no-cache-dir -r requirements.txt
 
 # Copy application files
-COPY src/transcribe_test.py /app/
+COPY src/transcribe_test.py data/test-audio-128kbps.mp3 /app/
 
-# Run app when the container launches, wrap with tini
-ENTRYPOINT ["tini", "--"]
-CMD ["python", "./transcribe_test.py"]
+# Set tini as the entry point and run transcript.py
+ENTRYPOINT ["/usr/bin/tini", "--", "python3", "/app/transcribe_test.py"]
--- src/transcribe_test.py
+++ src/transcribe_test.py
@@ -1,34 +1,109 @@
-import jax
-import jax.numpy as jnp
+import torch
+import whisperx
+import gc 
+import time
+import argparse
 
-from whisper_jax import FlaxWhisperPipline
-from jax.experimental.compilation_cache import compilation_cache as cc
-from datasets import load_dataset
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Transcript application.")
+
+    parser.add_argument(
+        "--audio-path", 
+        type=str,
+        required=False,
+        default="test-audio-128kbps.mp3", 
+        help="Path to an audio file.")
+    parser.add_argument(
+        "--model", 
+        type=str, 
+        choices=["small", "medium", "large-v1", "large-v2"],
+        required=False,
+        default="large-v2", 
+        help="Whisper model name.")
+    parser.add_argument(
+        "--hf-auth-token", 
+        type=str, 
+        required=False,
+        default=None, 
+        help="Auth token for the HuggingFace API.")
+    parser.add_argument(
+        "--compute-type",
+        type=str,
+        choices=["float16", "int8"],
+        required=False,
+        default="float16",
+        help="Model weight dtype.",
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        required=False,
+        default=16,
+        help="Batch size for batched inference",
+    )
+    args = parser.parse_args()
+
+    cuda_is_available = torch.cuda.is_available()
+    cuda_device_count = torch.cuda.device_count()
+    print(f"Torch CUDA is available: {cuda_is_available}, device count: {cuda_device_count}")
+    device = "cuda" if cuda_is_available else "cpu"
+
+    # 1. Transcribe with original whisper (batched)
+    model = whisperx.load_model("large-v2", device, compute_type=args.compute_type)
+
+    print(f"Reading audio file from: {args.audio_path}")
+    audio = whisperx.load_audio(args.audio_path)
+    print(f"Audio data: {audio}")
+
+    execution_times = {}
+
+    s_time_1 = time.time()
+    result = model.transcribe(audio, batch_size=args.batch_size)
+    t_time_1 = time.time() - s_time_1
+    execution_times["transcription"] = t_time_1
+    print(result["segments"]) # before alignment
+    print(f"Transcription took: {t_time_1} sec")
+
+    # delete model if low on GPU resources
+    # import gc; gc.collect(); torch.cuda.empty_cache(); del model
+
+    # 2. Align whisper output
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    s_time_2 = time.time()
+    result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+    t_time_2 = time.time() - s_time_2
+    execution_times["output alignment"] = t_time_2
+    print(result["segments"]) # after alignment
+    print(f"Whisper output alignment took: {t_time_2} sec")
+
+    # delete model if low on GPU resources
+    # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
+
+    # 3. Diarize and assign speaker labels
+    if args.hf_auth_token:
+        diarize_model = whisperx.DiarizationPipeline(use_auth_token=args.hf_auth_token, device=device)
+
+        # add min/max number of speakers if known
+        s_time_3 = time.time()
+        diarize_segments = diarize_model(audio)
+        # diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
+        t_time_3 = time.time() - s_time_3
+        execution_times["diarization"] = t_time_3
+        print(f"Diarization took: {t_time_3} sec")
+
+        s_time_4 = time.time()
+        result = whisperx.assign_word_speakers(diarize_segments, result)
+        t_time_4 = time.time() - s_time_4
+        execution_times["word speaker alignment"] = t_time_4
+        print(f"Word speaker assignment took: {t_time_4} sec")
+
+        print(diarize_segments)
+        print(result["segments"]) # segments are now assigned speaker IDs
+
+    print(f"Transcription took in total: {sum(execution_times.values())} sec, breakdown: {execution_times}")
 
 
-cc.initialize_cache("./jax_cache")
-print(f"JAX devices: {jax.devices()}")
-
-pipeline = FlaxWhisperPipline("openai/whisper-large-v2", dtype=jnp.bfloat16, batch_size=16)
-test_dataset = load_dataset("sanchit-gandhi/whisper-jax-test-files", split="train")
-
-# Load the second sample (5 mins) and get the audio
-# This is expected to take long because we are populating the JAX JIT cache
-audio_0 = test_dataset[0]["audio"]
-audio_0_length_in_mins = len(audio_0["array"]) / audio_0["sampling_rate"] / 60
-s_time = time.time()
-outputs_0 = pipeline(audio_0, return_timestamps=True)
-exec_time = time.time()-s_time
-print(f"Transcription #1 took {exec_time} sec(s), {(audio_0_length_in_mins*60)/exec_time} audio (sec)/wall time (sec)")
-text_0 = outputs_0["text"]
-
-# Load the second sample (30 mins) and get the audio array
-# This should be significantly faster as the JAX JIT cache is already populated
-audio_1 = test_dataset[1]["audio"]
-audio_1_length_in_mins = len(audio_1["array"]) / audio_1["sampling_rate"] / 60
-print(f"Transcribing audio of length {audio_1_length_in_mins} min(s)")
-s_time = time.time()
-outputs_1 = pipeline(audio_1, return_timestamps=True)
-exec_time = time.time()-s_time
-print(f"Transcription #2 took {exec_time} sec(s), {(audio_1_length_in_mins*60)/exec_time} audio (sec)/wall time (sec)")
-text_1 = outputs_1["text"] 
+if __name__ == "__main__":
+    main()
